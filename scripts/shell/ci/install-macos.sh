8a6842396ca14d1ae4fc3c73569b36500423b98c
Jonathan Merritt <j.s.merritt@gmail.com>
2017-04-22 22:34:39 +1000

    Only escape once

--- scripts/shell/ci/install-macos.sh
+++ scripts/shell/ci/install-macos.sh
@@ -11,8 +11,8 @@ echo "PROJECT_DIR=\"${PROJECT_DIR}\""
 LOCAL_CONFIG_FILE="${PROJECT_DIR}/LocalConfig.cmake"
 cat <<EOF > "${LOCAL_CONFIG_FILE}"
 set(Qt5_CMAKE_DIR "/usr/local/opt/qt5/lib/cmake")
-set(Qt5Core_DIR \\\${Qt5_CMAKE_DIR}/Qt5Core)
-set(Qt5OpenGL_DIR \\\${Qt5_CMAKE_DIR}/Qt5OpenGL)
+set(Qt5Core_DIR \${Qt5_CMAKE_DIR}/Qt5Core)
+set(Qt5OpenGL_DIR \${Qt5_CMAKE_DIR}/Qt5OpenGL)
 EOF
 echo "Contents of ${LOCAL_CONFIG_FILE}:"
 cat "${LOCAL_CONFIG_FILE}"
