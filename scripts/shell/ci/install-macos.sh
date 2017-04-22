36e953336eeca4acd72a452c2dcf5633ca22215e
Jonathan Merritt <j.s.merritt@gmail.com>
2017-04-22 22:26:20 +1000

    Dump extra build details

--- scripts/shell/ci/install-macos.sh
+++ scripts/shell/ci/install-macos.sh
@@ -4,17 +4,24 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PROJECT_DIR="${SCRIPT_DIR}/../../.."
+echo "SCRIPT_DIR=\"${SCRIPT_DIR}\""
+echo "PROJECT_DIR=\"${PROJECT_DIR}\""
 
 # Setup some paths for cmake in a LocalConfig file
-cat <<EOF > "${PROJECT_DIR}/LocalConfig.cmake"
+LOCAL_CONFIG_FILE="${PROJECT_DIR}/LocalConfig.cmake"
+cat <<EOF > "${LOCAL_CONFIG_FILE}"
 set(Qt5_CMAKE_DIR "/usr/local/opt/qt5/lib/cmake")
 set(Qt5Core_DIR ${Qt5_CMAKE_DIR}/Qt5Core)
 set(Qt5OpenGL_DIR ${Qt5_CMAKE_DIR}/Qt5OpenGL)
 EOF
+echo "Contents of ${LOCAL_CONFIG_FILE}:"
+cat "${LOCAL_CONFIG_FILE}"
+echo "End of ${LOCAL_CONFIG_FILE}"
 
 # Do the build in a subdirectory
-mkdir "${PROJECT_DIR}/build"
-pushd "${PROJECT_DIR}/build"
+BUILD_DIR="${PROJECT_DIR}/build"
+echo "Building in ${BUILD_DIR}"
+mkdir -p "${BUILD_DIR}" && pushd "${BUILD_DIR}"
 cmake "${PROJECT_DIR}"
 make
 popd
